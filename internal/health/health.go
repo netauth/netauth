20b1fce21fa658ab3090c806b23fea68cd87096c
Michael Aldridge <aldridge.mac@gmail.com>
2018-07-01 00:53:11 -0700

    internal/health: address lint concerns

--- internal/health/health.go
+++ internal/health/health.go
@@ -7,18 +7,21 @@ import (
 
 // The server starts out unhealthy by default, since it hasn't loaded
 // any data and hasn't expressed readiness to serve.
-var healthy int32 = 0
+var healthy int32
 
+// SetGood sets the overall status of the server to be OK.
 func SetGood() {
 	atomic.StoreInt32(&healthy, 1)
 	log.Println("Server health status GOOD")
 }
 
+// SetBad sets the overall status of the server to be BAD.
 func SetBad() {
 	atomic.StoreInt32(&healthy, 0)
 	log.Println("Server health status BAD")
 }
 
+// Get returns whether the server is OK or not.
 func Get() bool {
 	return atomic.LoadInt32(&healthy) != 0
 }
