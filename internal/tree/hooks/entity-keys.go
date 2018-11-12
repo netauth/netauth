e67fce0d7627f86a460ddf080e6be498fe8ea657
Michael Aldridge <aldridge.mac@gmail.com>
2018-11-11 23:41:48 -0800

    internal/tree/hooks: ManageEntityKeys - gofmt/tests/comments

--- internal/tree/hooks/entity-keys.go
+++ internal/tree/hooks/entity-keys.go
@@ -7,14 +7,17 @@ import (
 	pb "github.com/NetAuth/Protocol"
 )
 
+// ManageEntityKeys is a configurable hook that adds and removes keys.
 type ManageEntityKeys struct {
 	tree.BaseHook
 	mode bool
 }
 
+// Run iterates on all keys in the request and adds or removes them
+// from the entity's keystore.
 func (mek *ManageEntityKeys) Run(e, de *pb.Entity) error {
 	for _, k := range de.Meta.Keys {
-		e.Meta.Keys = util.PatchStringSlice(e.Meta.Keys, k, true, mek.mode)
+		e.Meta.Keys = util.PatchStringSlice(e.Meta.Keys, k, mek.mode, false)
 	}
 	return nil
 }
@@ -24,10 +27,12 @@ func init() {
 	tree.RegisterEntityHookConstructor("del-entity-key", NewDelEntityKey)
 }
 
+// NewAddEntityKey returns a hook initialized for adding keys.
 func NewAddEntityKey(c tree.RefContext) (tree.EntityProcessorHook, error) {
 	return &ManageEntityKeys{tree.NewBaseHook("add-entity-key", 50), true}, nil
 }
 
+// NewDelEntityKey returns a hook initialized for removing keys.
 func NewDelEntityKey(c tree.RefContext) (tree.EntityProcessorHook, error) {
 	return &ManageEntityKeys{tree.NewBaseHook("del-entity-key", 50), false}, nil
 }
---
+++ internal/tree/hooks/entity-keys_test.go
@@ -0,0 +1,58 @@
+package hooks
+
+import (
+	"testing"
+
+	"github.com/NetAuth/NetAuth/internal/tree"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+func TestAddEntityKey(t *testing.T) {
+	hook, err := NewAddEntityKey(tree.RefContext{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := &pb.Entity{Meta: &pb.EntityMeta{}}
+	de := &pb.Entity{
+		Meta: &pb.EntityMeta{
+			Keys: []string{"KEY:code<>"},
+		},
+	}
+
+	if err := hook.Run(e, de); err != nil {
+		t.Fatal(err)
+	}
+
+	if e.GetMeta().GetKeys()[0] != "KEY:code<>" {
+		t.Fatal("Spec error - please trace plugin")
+	}
+}
+
+func TestDelEntityKey(t *testing.T) {
+	hook, err := NewDelEntityKey(tree.RefContext{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := &pb.Entity{
+		Meta: &pb.EntityMeta{
+			Keys: []string{"KEY:code<>", "KEY2:code<>"},
+		},
+	}
+	de := &pb.Entity{
+		Meta: &pb.EntityMeta{
+			Keys: []string{"KEY:code<>"},
+		},
+	}
+
+	if err := hook.Run(e, de); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(e.GetMeta().GetKeys()) != 1 {
+		t.Log(e)
+		t.Fatal("Spec error - please trace plugin")
+	}
+}
