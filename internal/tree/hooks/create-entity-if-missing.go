27e0addfcfc2f62dc48ed6e6f47fc34ad6090642
Michael Aldridge <aldridge.mac@gmail.com>
2018-11-11 21:44:07 -0800

    internal/tree/hooks: CreateEntityIfMissing - gofmt/tests/comments

--- internal/tree/hooks/create-entity-if-missing.go
+++ internal/tree/hooks/create-entity-if-missing.go
@@ -8,11 +8,20 @@ import (
 	pb "github.com/NetAuth/Protocol"
 )
 
+// CreateEntityIfMissing is an EntityProcessor hook that will ensure
+// that e exists and is populated before returning.  This hook is
+// primarily used for bootstrap actions where an entity needs to
+// either exist or be created and it isn't important which of these
+// happens.
 type CreateEntityIfMissing struct {
 	tree.BaseHook
 	db.DB
 }
 
+// Run will attempt to load the entity from an external source.  If
+// the load returns that the failure is due to an unknown entity, then
+// one will be created.  Any other load failure will result in an
+// error being returned.  Returned errors will be of a db.* type.
 func (c *CreateEntityIfMissing) Run(e, de *pb.Entity) error {
 	le, err := c.LoadEntity(de.GetID())
 	switch err {
@@ -36,6 +45,8 @@ func init() {
 	tree.RegisterEntityHookConstructor("create-entity-if-missing", NewCreateEntityIfMissing)
 }
 
+// NewCreateEntityIfMissing returns an initalized hook for use during
+// tree initialization.
 func NewCreateEntityIfMissing(c tree.RefContext) (tree.EntityProcessorHook, error) {
 	return &CreateEntityIfMissing{tree.NewBaseHook("create-entity-if-missing", 1), c.DB}, nil
 }
---
+++ internal/tree/hooks/create-entity-if-missing_test.go
@@ -0,0 +1,76 @@
+package hooks
+
+import (
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/NetAuth/NetAuth/internal/db/memdb"
+	"github.com/NetAuth/NetAuth/internal/tree"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+func TestLoadExisting(t *testing.T) {
+	mdb, err := memdb.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx := tree.RefContext{
+		DB: mdb,
+	}
+
+	hook, err := NewCreateEntityIfMissing(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = mdb.SaveEntity(&pb.Entity{
+		ID:     proto.String("foo"),
+		Number: proto.Int32(42),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := &pb.Entity{}
+	de := &pb.Entity{
+		ID: proto.String("foo"),
+	}
+
+	if err := hook.Run(e, de); err != nil {
+		t.Fatal(err)
+	}
+
+	if e.GetNumber() != 42 {
+		t.Fatal("Existing entity not retrieved")
+	}
+
+}
+
+func TestCreateNew(t *testing.T) {
+	mdb, err := memdb.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx := tree.RefContext{
+		DB: mdb,
+	}
+
+	hook, err := NewCreateEntityIfMissing(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := &pb.Entity{}
+	de := &pb.Entity{
+		ID: proto.String("foo"),
+	}
+
+	if err := hook.Run(e, de); err != nil {
+		t.Fatal(err)
+	}
+	if e.GetID() != "foo" {
+		t.Fatal("Bad Entity")
+	}
+}
