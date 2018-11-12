7ff87f1829aa624b326c6eafa62e4148592e48fc
Michael Aldridge <aldridge.mac@gmail.com>
2018-11-11 22:04:45 -0800

    internal/tree/hooks: ManageEntityCapabilities - gofmt/tests/comments

--- internal/tree/hooks/entity-capabilities.go
+++ internal/tree/hooks/entity-capabilities.go
@@ -6,24 +6,33 @@ import (
 	pb "github.com/NetAuth/Protocol"
 )
 
+// ManageEntityCapabilities is a configurable runtime hook that adds
+// or removes capabilities as configured.  Two convenience
+// constructors exist to return hooks in either mode.
 type ManageEntityCapabilities struct {
 	tree.BaseHook
 	mode bool
 }
 
+// Run modifies the stored capabilities on an entity depending on the
+// value of the mode variable.  When the mode is set to true, any
+// capabilities stored in de will be copied to e if they are not
+// already present.  In false capabilities will be subtracted.
 func (mec *ManageEntityCapabilities) Run(e, de *pb.Entity) error {
 	for _, cap := range de.Meta.Capabilities {
 		if mec.mode {
 			// Add mode
-			de.Meta.Capabilities = mec.addCapability(cap, de.Meta.Capabilities)
+			e.Meta.Capabilities = mec.addCapability(cap, e.Meta.Capabilities)
 		} else {
 			// Del mode
-			de.Meta.Capabilities = mec.delCapability(cap, de.Meta.Capabilities)
+			e.Meta.Capabilities = mec.delCapability(cap, e.Meta.Capabilities)
 		}
 	}
 	return nil
 }
 
+// addCapability is an internal convenience function to add
+// capabilities if they do not already exist in a capability slice.
 func (mec *ManageEntityCapabilities) addCapability(cap pb.Capability, caps []pb.Capability) []pb.Capability {
 	var ncaps []pb.Capability
 
@@ -36,11 +45,13 @@ func (mec *ManageEntityCapabilities) addCapability(cap pb.Capability, caps []pb.
 	}
 
 	// Add the new capability to the list.
-	ncaps = []pb.Capability{cap}
+	ncaps = append(ncaps, cap)
 
 	return ncaps
 }
 
+// delCapability is an internal convenience function to remove
+// capabilities that exist in a slice.
 func (mec *ManageEntityCapabilities) delCapability(cap pb.Capability, caps []pb.Capability) []pb.Capability {
 	var ncaps []pb.Capability
 	for _, a := range caps {
@@ -58,10 +69,14 @@ func init() {
 	tree.RegisterEntityHookConstructor("remove-entity-capability", NewRemoveEntityCapability)
 }
 
+// NewSetEntityCapability returns a ManageEntityCapability hook
+// pre-configured into the additive mode.
 func NewSetEntityCapability(c tree.RefContext) (tree.EntityProcessorHook, error) {
 	return &ManageEntityCapabilities{tree.NewBaseHook("set-entity-capability", 50), true}, nil
 }
 
+// NewRemoveEntityCapability returns a ManageEntityCapability hook
+// pre-configured into the subtractive mode.s
 func NewRemoveEntityCapability(c tree.RefContext) (tree.EntityProcessorHook, error) {
 	return &ManageEntityCapabilities{tree.NewBaseHook("remove-entity-capability", 50), false}, nil
 }
---
+++ internal/tree/hooks/entity-capabilities_test.go
@@ -0,0 +1,70 @@
+package hooks
+
+import (
+	"testing"
+
+	"github.com/NetAuth/NetAuth/internal/tree"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+func TestAddCapabilities(t *testing.T) {
+	hook, err := NewSetEntityCapability(tree.RefContext{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := &pb.Entity{Meta: &pb.EntityMeta{}}
+	de := &pb.Entity{
+		Meta: &pb.EntityMeta{
+			Capabilities: []pb.Capability{
+				pb.Capability_CREATE_ENTITY,
+				pb.Capability_CREATE_ENTITY,
+				pb.Capability_CREATE_GROUP,
+			},
+		},
+	}
+
+	if err := hook.Run(e, de); err != nil {
+		t.Fatal(err)
+	}
+
+	caps := e.GetMeta().GetCapabilities()
+	if len(caps) != 2 || caps[0] != pb.Capability_CREATE_ENTITY {
+		t.Log(e)
+		t.Fatal("Spec failure, please examine hook execution")
+	}
+}
+
+func TestRemoveCapabilities(t *testing.T) {
+	hook, err := NewRemoveEntityCapability(tree.RefContext{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := &pb.Entity{
+		Meta: &pb.EntityMeta{
+			Capabilities: []pb.Capability{
+				pb.Capability_CREATE_ENTITY,
+				pb.Capability_CREATE_GROUP,
+			},
+		},
+	}
+	de := &pb.Entity{
+		Meta: &pb.EntityMeta{
+			Capabilities: []pb.Capability{
+				pb.Capability_CREATE_ENTITY,
+			},
+		},
+	}
+
+	if err := hook.Run(e, de); err != nil {
+		t.Fatal(err)
+	}
+
+	caps := e.GetMeta().GetCapabilities()
+	if len(caps) != 1 || caps[0] != pb.Capability_CREATE_GROUP {
+		t.Log(e)
+		t.Fatal("Spec failure, please examine hook execution")
+	}
+}
