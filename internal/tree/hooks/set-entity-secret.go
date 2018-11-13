0d4298d305344e1f2dfc202c555afb13fdd3dc32
Michael Aldridge <aldridge.mac@gmail.com>
2018-11-13 00:48:44 -0800

    internal/tree/hooks: SetEntitySecret - gofmt/tests/comments

--- internal/tree/hooks/set-entity-secret.go
+++ internal/tree/hooks/set-entity-secret.go
@@ -7,11 +7,15 @@ import (
 	pb "github.com/NetAuth/Protocol"
 )
 
+// SetEntitySecret takes a plaintext secret and converts it to a
+// secured secret for storage.
 type SetEntitySecret struct {
 	tree.BaseHook
 	crypto.EMCrypto
 }
 
+// Run takes a plaintext secret from de.Secret and secures it using a
+// crypto engine.  The secured secret will be written to e.Secret.
 func (s *SetEntitySecret) Run(e, de *pb.Entity) error {
 	ssecret, err := s.SecureSecret(de.GetSecret())
 	if err != nil {
@@ -25,6 +29,7 @@ func init() {
 	tree.RegisterEntityHookConstructor("set-entity-secret", NewSetEntitySecret)
 }
 
+// NewSetEntitySecret returns an initialized hook for use.
 func NewSetEntitySecret(c tree.RefContext) (tree.EntityProcessorHook, error) {
 	return &SetEntitySecret{tree.NewBaseHook("set-entity-secret", 50), c.Crypto}, nil
 }
---
+++ internal/tree/hooks/set-entity-secret_test.go
@@ -0,0 +1,36 @@
+package hooks
+
+import (
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/NetAuth/NetAuth/internal/crypto/nocrypto"
+	"github.com/NetAuth/NetAuth/internal/tree"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+func TestSetEntitySecret(t *testing.T) {
+	crypt, err := nocrypto.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	hook, err := NewSetEntitySecret(tree.RefContext{Crypto: crypt})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := &pb.Entity{}
+	de := &pb.Entity{Secret: proto.String("security")}
+
+	if err := hook.Run(e, de); err != nil {
+		t.Fatal(err)
+	}
+
+	if e.GetSecret() != "security" {
+		t.Log(e)
+		t.Fatal("Spec error - please trace hook")
+	}
+}
