dc49ca8a0509096465a7d15fa1ad6ad752a0429a
Michael Aldridge <aldridge.mac@gmail.com>
2018-11-12 23:09:21 -0800

    internal/tree/hooks: EntityLockManager - gofmt/tests/comments

--- internal/tree/hooks/entity-locks.go
+++ internal/tree/hooks/entity-locks.go
@@ -7,11 +7,15 @@ import (
 	pb "github.com/NetAuth/Protocol"
 )
 
+// The EntityLockManager is a configurable hook that can either lock
+// or unlock entities as needed.
 type EntityLockManager struct {
 	tree.BaseHook
 	lockstate bool
 }
 
+// Run will set the entity lock status unconditionally to the
+// configured value for the instantiated hook.
 func (elm *EntityLockManager) Run(e, de *pb.Entity) error {
 	e.Meta.Locked = proto.Bool(elm.lockstate)
 	return nil
@@ -22,10 +26,12 @@ func init() {
 	tree.RegisterEntityHookConstructor("unlock-entity", NewELMUnlock)
 }
 
+// NewELMLock returns a configured hook in LOCK mode.
 func NewELMLock(c tree.RefContext) (tree.EntityProcessorHook, error) {
 	return &EntityLockManager{tree.NewBaseHook("lock-entity", 40), true}, nil
 }
 
+// NewELMUnlock returns a configured hook in UNLOCK mode.
 func NewELMUnlock(c tree.RefContext) (tree.EntityProcessorHook, error) {
 	return &EntityLockManager{tree.NewBaseHook("unlock-entity", 40), false}, nil
 }
---
+++ internal/tree/hooks/entity-locks_test.go
@@ -0,0 +1,47 @@
+package hooks
+
+import (
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/NetAuth/NetAuth/internal/tree"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+func TestEntityLock(t *testing.T) {
+	hook, err := NewELMLock(tree.RefContext{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := &pb.Entity{Meta: &pb.EntityMeta{}}
+	de := &pb.Entity{}
+
+	if err := hook.Run(e, de); err != nil {
+		t.Fatal(err)
+	}
+
+	if !e.GetMeta().GetLocked() {
+		t.Fatal("Spec error - please trace hook")
+	}
+}
+
+func TestEntityUnLock(t *testing.T) {
+	hook, err := NewELMUnlock(tree.RefContext{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := &pb.Entity{Meta: &pb.EntityMeta{Locked: proto.Bool(true)}}
+	de := &pb.Entity{}
+
+	if err := hook.Run(e, de); err != nil {
+		t.Fatal(err)
+	}
+
+	if e.GetMeta().GetLocked() {
+		t.Fatal("Spec error - please trace hook")
+	}
+}
