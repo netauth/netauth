41a0c6ae519d51f4be98a1d3698e4d04fde34a06
Michael Aldridge <aldridge.mac@gmail.com>
2018-11-12 20:26:09 -0800

    internal/tree/hooks: ManageEntityUM - gofmt/tests/comments

--- internal/tree/hooks/untyped-entity-meta.go
+++ internal/tree/hooks/untyped-entity-meta.go
@@ -9,11 +9,17 @@ import (
 	pb "github.com/NetAuth/Protocol"
 )
 
+// ManageEntityUM is a configurable plugin that manages the untyped
+// metadata for entities.
 type ManageEntityUM struct {
 	tree.BaseHook
 	mode string
 }
 
+// Run will process metadata provided via de onto e according to the
+// mode the plugin is configured for.  "UPSERT" will add or update
+// fields as appropriate.  "CLEARFUZZY" will ignore Z-Indexing
+// annotations.  "CLEAREXACT" will require exact key specifications.
 func (mm *ManageEntityUM) Run(e, de *pb.Entity) error {
 	for _, m := range de.Meta.UntypedMeta {
 		key, value := splitKeyValue(m)
@@ -33,14 +39,17 @@ func init() {
 	tree.RegisterEntityHookConstructor("del-untyped-metadata-exact", NewDelExactEntityUM)
 }
 
+// NewAddEntityUM returns a configured hook in UPSERT mode.
 func NewAddEntityUM(c tree.RefContext) (tree.EntityProcessorHook, error) {
 	return &ManageEntityUM{tree.NewBaseHook("add-untyped-metadata", 50), "UPSERT"}, nil
 }
 
+// NewDelFuzzyEntityUM returns a configured hook in CLEARFUZZY mode.
 func NewDelFuzzyEntityUM(c tree.RefContext) (tree.EntityProcessorHook, error) {
 	return &ManageEntityUM{tree.NewBaseHook("del-untyped-metadata-fuzzy", 50), "CLEARFUZZY"}, nil
 }
 
+// NewDelExactentityUM returns a configured hook in CLEAREXACT mode.
 func NewDelExactEntityUM(c tree.RefContext) (tree.EntityProcessorHook, error) {
-	return &ManageEntityUM{tree.NewBaseHook("del-untyped-metadata-exact", 50), "UPSERT"}, nil
+	return &ManageEntityUM{tree.NewBaseHook("del-untyped-metadata-exact", 50), "CLEAREXACT"}, nil
 }
---
+++ internal/tree/hooks/untyped-entity-meta_test.go
@@ -0,0 +1,91 @@
+package hooks
+
+import (
+	"testing"
+
+	"github.com/NetAuth/NetAuth/internal/tree"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+func TestAddEntityUM(t *testing.T) {
+	hook, err := NewAddEntityUM(tree.RefContext{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := &pb.Entity{Meta: &pb.EntityMeta{}}
+	de := &pb.Entity{
+		Meta: &pb.EntityMeta{
+			UntypedMeta: []string{"key:value:with:colons"},
+		},
+	}
+
+	if err := hook.Run(e, de); err != nil {
+		t.Fatal(err)
+	}
+
+	if e.GetMeta().GetUntypedMeta()[0] != "key:value:with:colons" {
+		t.Fatal("Spec error - please trace hook")
+	}
+}
+
+func TestDelFuzzyEntityUM(t *testing.T) {
+	hook, err := NewDelFuzzyEntityUM(tree.RefContext{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := &pb.Entity{
+		Meta: &pb.EntityMeta{
+			UntypedMeta: []string{
+				"key{0}:value",
+				"key{1}:value1",
+			},
+		},
+	}
+	de := &pb.Entity{
+		Meta: &pb.EntityMeta{
+			UntypedMeta: []string{"key:"},
+		},
+	}
+
+	if err := hook.Run(e, de); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(e.GetMeta().GetUntypedMeta()) != 0 {
+		t.Log(e)
+		t.Fatal("Spec error - please trace hook")
+	}
+}
+
+func TestDelExactEntityUM(t *testing.T) {
+	hook, err := NewDelExactEntityUM(tree.RefContext{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e := &pb.Entity{
+		Meta: &pb.EntityMeta{
+			UntypedMeta: []string{
+				"key{0}:value",
+				"key{1}:value1",
+			},
+		},
+	}
+	de := &pb.Entity{
+		Meta: &pb.EntityMeta{
+			UntypedMeta: []string{"key{1}:"},
+		},
+	}
+
+	if err := hook.Run(e, de); err != nil {
+		t.Fatal(err)
+	}
+
+	if e.GetMeta().GetUntypedMeta()[0] != "key{0}:value" {
+		t.Log(e)
+		t.Fatal("Spec error - please trace hook")
+	}
+}
