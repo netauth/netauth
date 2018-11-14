3f513b30350b667c5e1afe9b17d87a81fdf2a58b
Michael Aldridge <aldridge.mac@gmail.com>
2018-11-14 00:10:58 -0800

    internal/tree/hooks: SetGroupNumber - gofmt/tests/comments

--- internal/tree/hooks/set-group-number.go
+++ internal/tree/hooks/set-group-number.go
@@ -2,16 +2,25 @@ package hooks
 
 import (
 	"github.com/NetAuth/NetAuth/internal/db"
+	"github.com/NetAuth/NetAuth/internal/tree"
 
 	pb "github.com/NetAuth/Protocol"
 )
 
+// SetGroupNumber assigns a group number either by using the
+// statically provided one, or dynamically requesting one from the
+// database.
 type SetGroupNumber struct {
+	tree.BaseHook
 	db.DB
 }
 
-func (*SetGroupNumber) Name() string { return "set-group-number" }
-func (*SetGroupNumber) Priority() int { return 50 }
+// Run will set the group number on g.  If dg.Number is provided as a
+// non-zero positive integer, it will be used directly (care should be
+// taken this number is not already allocated).  If dg.Number is -1, a
+// number will be dynamically provisioned by the database.  It is
+// recommended to use automatic provisioning unless strictly necessary
+// to do otherwise.
 func (s *SetGroupNumber) Run(g, dg *pb.Group) error {
 	if dg.GetNumber() == -1 {
 		number, err := s.NextGroupNumber()
@@ -21,6 +30,15 @@ func (s *SetGroupNumber) Run(g, dg *pb.Group) error {
 		g.Number = &number
 		return nil
 	}
-	dg.Number = g.Number
+	g.Number = dg.Number
 	return nil
+}
+
+func init() {
+	tree.RegisterGroupHookConstructor("set-group-number", NewSetGroupNumber)
+}
+
+// NewSetGroupNumber returns a hook initialized and ready for use.
+func NewSetGroupNumber(c tree.RefContext) (tree.GroupProcessorHook, error) {
+	return &SetGroupNumber{tree.NewBaseHook("set-group-number", 50), c.DB}, nil
 }
---
+++ internal/tree/hooks/set-group-number_test.go
@@ -0,0 +1,44 @@
+package hooks
+
+import (
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/NetAuth/NetAuth/internal/db/memdb"
+	"github.com/NetAuth/NetAuth/internal/tree"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+func TestSetGroupNumber(t *testing.T) {
+	db, err := memdb.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	hook, err := NewSetGroupNumber(tree.RefContext{DB: db})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	g := &pb.Group{}
+
+	if err := hook.Run(g, &pb.Group{Number: proto.Int32(27)}); err != nil {
+		t.Fatal(err)
+	}
+
+	if g.GetNumber() != 27 {
+		t.Log(g)
+		t.Error("Spec failure - please trace hook")
+	}
+
+	if err := hook.Run(g, &pb.Group{Number: proto.Int32(-1)}); err != nil {
+		t.Fatal(err)
+	}
+
+	if g.GetNumber() != 1 {
+		t.Log(g)
+		t.Error("Spec failure = please trace hook")
+	}
+}
