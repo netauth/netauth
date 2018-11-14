bebce63d9c0fbfa269ed492642b59c5c6cf15e04
Michael Aldridge <aldridge.mac@gmail.com>
2018-11-13 23:35:15 -0800

    internal/tree/hooks: SetGroupName - gofmt/tests/comments

--- internal/tree/hooks/set-group-name.go
+++ internal/tree/hooks/set-group-name.go
@@ -1,14 +1,28 @@
 package hooks
 
 import (
+	"github.com/NetAuth/NetAuth/internal/tree"
+
 	pb "github.com/NetAuth/Protocol"
 )
 
-type SetGroupName struct {}
+// SetGroupName copies the name from dg to g.
+type SetGroupName struct {
+	tree.BaseHook
+}
 
-func (*SetGroupName) Name() string { return "set-group-name" }
-func (*SetGroupName) Priority() int { return 50 }
+// Run sets the name on g to the name on dg, no checks or validation
+// are run during this hook.
 func (*SetGroupName) Run(g, dg *pb.Group) error {
 	g.Name = dg.Name
 	return nil
+}
+
+func init() {
+	tree.RegisterGroupHookConstructor("set-group-name", NewSetGroupName)
+}
+
+// NewSetGroupName returns an initialized hook.
+func NewSetGroupName(c tree.RefContext) (tree.GroupProcessorHook, error) {
+	return &SetGroupName{tree.NewBaseHook("set-group-name", 50)}, nil
 }
---
+++ internal/tree/hooks/set-group-name_test.go
@@ -0,0 +1,30 @@
+package hooks
+
+import (
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/NetAuth/NetAuth/internal/tree"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+func TestSetGroupName(t *testing.T) {
+	hook, err := NewSetGroupName(tree.RefContext{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	g := &pb.Group{}
+	dg := &pb.Group{Name: proto.String("fooGroup")}
+
+	if err := hook.Run(g, dg); err != nil {
+		t.Fatal(err)
+	}
+
+	if g.GetName() != "fooGroup" {
+		t.Log(g)
+		t.Error("Spec error - please trace hook")
+	}
+}
