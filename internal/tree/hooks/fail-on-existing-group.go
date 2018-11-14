b5dda79a918dc423d8561e8d7bdd806fe8152d3b
Michael Aldridge <aldridge.mac@gmail.com>
2018-11-13 21:39:24 -0800

    internal/tree/hooks: FailOnExistingGroup - gofmt/tests/comments

--- internal/tree/hooks/fail-on-existing-group.go
+++ internal/tree/hooks/fail-on-existing-group.go
@@ -7,17 +7,28 @@ import (
 	pb "github.com/NetAuth/Protocol"
 )
 
+// FailOnExistingGroup is a hook that can be used to guard creation
+// processes on groups.
 type FailOnExistingGroup struct {
+	tree.BaseHook
 	db.DB
 }
 
-func (*FailOnExistingGroup) Name() string  { return "fail-on-existing-group" }
-func (*FailOnExistingGroup) Priority() int { return 0 }
+// Run contacts the datastore and attempts to load the group specified
+// by dg.  If the group loads successfully then an error is returned,
+// in other cases nil is returned.
 func (f *FailOnExistingGroup) Run(g, dg *pb.Group) error {
 	if _, err := f.LoadGroup(dg.GetName()); err == nil {
 		return tree.ErrDuplicateGroupName
-	} else if err != db.ErrUnknownGroup {
-		return err
 	}
 	return nil
+}
+
+func init() {
+	tree.RegisterGroupHookConstructor("fail-on-existing-group", NewFailOnExistingGroup)
+}
+
+// NewFailOnExistingGroup returns an initialized hook ready for use.
+func NewFailOnExistingGroup(c tree.RefContext) (tree.GroupProcessorHook, error) {
+	return &FailOnExistingGroup{tree.NewBaseHook("fail-on-existing-group", 0), c.DB}, nil
 }
---
+++ internal/tree/hooks/fail-on-existing-group_test.go
@@ -0,0 +1,49 @@
+package hooks
+
+import (
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/NetAuth/NetAuth/internal/db/memdb"
+	"github.com/NetAuth/NetAuth/internal/tree"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+func TestFailOnExistingGroup(t *testing.T) {
+	mdb, err := memdb.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx := tree.RefContext{
+		DB: mdb,
+	}
+
+	hook, err := NewFailOnExistingGroup(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = mdb.SaveGroup(&pb.Group{
+		Name: proto.String("foo"),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cases := []struct {
+		name    string
+		wantErr error
+	}{
+		{"foo", tree.ErrDuplicateGroupName},
+		{"bar", nil},
+	}
+	for i, c := range cases {
+		g := &pb.Group{}
+		dg := &pb.Group{Name: &c.name}
+		if err := hook.Run(g, dg); err != c.wantErr {
+			t.Errorf("Case %d: Got: %v Want: %v", i, err, c.wantErr)
+		}
+	}
+}
