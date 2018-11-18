d20a108fd18f62031ea3f25a8e95f1fe9b92b456
Michael Aldridge <aldridge.mac@gmail.com>
2018-11-17 18:31:20 -0800

    internal/tree/hooks: DestroyGroup - gofmt/tests/comments

---
+++ internal/tree/hooks/destroy-group.go
@@ -0,0 +1,35 @@
+package hooks
+
+import (
+	"github.com/NetAuth/NetAuth/internal/db"
+	"github.com/NetAuth/NetAuth/internal/tree"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+// DestroyGroup removes an entity from the system.
+type DestroyGroup struct {
+	tree.BaseHook
+	db.DB
+}
+
+// Run will request the underlying datastore to remove the group,
+// returning any status provided.  If the group Name is not specified
+// in g, it will be obtained from dg.
+func (d *DestroyGroup) Run(g, dg *pb.Group) error {
+	// This hook is somewhat special since it might be called
+	// after a processing pipeline, or just to remove a group.
+	if g.GetName() == "" {
+		g.Name = dg.Name
+	}
+	return d.DeleteGroup(g.GetName())
+}
+
+func init() {
+	tree.RegisterGroupHookConstructor("destroy-group", NewDestroyGroup)
+}
+
+// NewDestroyGroup returns an initialized DestroyGroup hook for use.
+func NewDestroyGroup(c tree.RefContext) (tree.GroupProcessorHook, error) {
+	return &DestroyGroup{tree.NewBaseHook("destroy-group", 99), c.DB}, nil
+}
---
+++ internal/tree/hooks/destroy-group_test.go
@@ -0,0 +1,44 @@
+package hooks
+
+import (
+	"testing"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/NetAuth/NetAuth/internal/db/memdb"
+	"github.com/NetAuth/NetAuth/internal/tree"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+func TestDestroyGroup(t *testing.T) {
+	mdb, err := memdb.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx := tree.RefContext{
+		DB: mdb,
+	}
+
+	hook, err := NewDestroyGroup(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err = mdb.SaveGroup(&pb.Group{Name: proto.String("foo")}); err != nil {
+		t.Fatal(err)
+	}
+	if err = mdb.SaveGroup(&pb.Group{Name: proto.String("bar")}); err != nil {
+		t.Fatal(err)
+	}
+
+	// Act as though a delete was requested normally
+	if err := hook.Run(&pb.Group{}, &pb.Group{Name: proto.String("foo")}); err != nil {
+		t.Fatal(err)
+	}
+
+	// Act as though deleting an entity at the end of a pipeline
+	if err := hook.Run(&pb.Group{Name: proto.String("bar")}, &pb.Group{}); err != nil {
+		t.Fatal(err)
+	}
+}
