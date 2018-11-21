81af6f06191e1b0a38f61e6e89d8cfdd18d70b37
Michael Aldridge <aldridge.mac@gmail.com>
2018-11-20 19:53:48 -0800

    internal/tree/hooks: PatchGroupExpansions - Add Hook

---
+++ internal/tree/hooks/patch-group-expansions.go
@@ -0,0 +1,48 @@
+package hooks
+
+import (
+	"sort"
+	"strings"
+
+	"github.com/NetAuth/NetAuth/internal/tree"
+	"github.com/NetAuth/NetAuth/internal/tree/util"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+// PatchGroupExpansions handles the application of group expansions.
+// Checks that verify the referential integrity of these expansions
+// should have already run before executing this hook.
+type PatchGroupExpansions struct {
+	tree.BaseHook
+}
+
+// Run iterates through the expansions in dg and applies them to g.
+// DROP expansions are processed with fuzzy group name matching.
+func (*PatchGroupExpansions) Run(g, dg *pb.Group) error {
+	exps := dg.GetExpansions()
+	for i := range exps {
+		parts := strings.SplitN(exps[i], ":", 2)
+		if parts[0] == "INCLUDE" || parts[0] == "EXCLUDE" {
+			g.Expansions = util.PatchStringSlice(g.Expansions, exps[i], true, true)
+		} else if parts[0] == "DROP" {
+			// Patch out with fuzzy matching, this will
+			// innevitably come back to bite someone, but
+			// until then its much faster than doing
+			// strict checking on the group name.
+			g.Expansions = util.PatchStringSlice(g.Expansions, parts[1], false, false)
+		}
+	}
+	sort.Strings(g.Expansions)
+
+	return nil
+}
+
+func init() {
+	tree.RegisterGroupHookConstructor("patch-group-expansions", NewPatchGroupExpansions)
+}
+
+// NewPatchGroupExpansions returns an initialized hook for use.
+func NewPatchGroupExpansions(tree.RefContext) (tree.GroupProcessorHook, error) {
+	return &PatchGroupExpansions{tree.NewBaseHook("patch-group-expansions", 50)}, nil
+}
---
+++ internal/tree/hooks/patch-group-expansions_test.go
@@ -0,0 +1,59 @@
+package hooks
+
+import (
+	"testing"
+
+	"github.com/NetAuth/NetAuth/internal/tree"
+
+	pb "github.com/NetAuth/Protocol"
+)
+
+func TestPatchGroupExpansionsInclude(t *testing.T) {
+	hook, err := NewPatchGroupExpansions(tree.RefContext{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	g := &pb.Group{}
+	dg := &pb.Group{
+		Expansions: []string{
+			"INCLUDE:group1",
+		},
+	}
+
+	if err := hook.Run(g, dg); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(g.GetExpansions()) != 1 || g.GetExpansions()[0] != "INCLUDE:group1" {
+		t.Error("Spec error - please trace hook")
+	}
+}
+
+func TestPatchGroupExpansionsDrop(t *testing.T) {
+	hook, err := NewPatchGroupExpansions(tree.RefContext{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	g := &pb.Group{
+		Expansions: []string{
+			"INCLUDE:group1",
+			"EXCLUDE:group2",
+		},
+	}
+	dg := &pb.Group{
+		Expansions: []string{
+			"DROP:group1",
+		},
+	}
+
+	if err := hook.Run(g, dg); err != nil {
+		t.Fatal(err)
+	}
+
+	if len(g.GetExpansions()) != 1 || g.GetExpansions()[0] != "EXCLUDE:group2" {
+		t.Error("Spec error - please trace hook")
+	}
+
+}
