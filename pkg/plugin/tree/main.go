433796fba75ef6584625b335052734fad886bfe9
Michael Aldridge <aldridge.mac@gmail.com>
2019-09-07 18:59:11 -0700

    pkg/plugin: Add alias to internal plugin type

--- pkg/plugin/tree/main.go
+++ pkg/plugin/tree/main.go
@@ -5,8 +5,12 @@ import (
 	"github.com/NetAuth/NetAuth/internal/plugin/tree/common"
 )
 
+// Plugin is an alias to an internal type that all tree modifying
+// plugins must satisfy.
+type Plugin = common.Plugin
+
 // PluginMain is called with an interface to serve as the plugin.
 // This function never returns.
-func PluginMain(i common.Plugin) {
+func PluginMain(i Plugin) {
 	provider.Main(i)
 }
