b25ed8f065c5cf4aaf146adfd87dacbc156bb5ed
Michael Aldridge <aldridge.mac@gmail.com>
2019-09-25 00:12:42 -0700

    internal/token: Add null implementation

---
+++ internal/token/null/null.go
@@ -0,0 +1,45 @@
+package null
+
+import (
+	"encoding/json"
+
+	"github.com/NetAuth/NetAuth/internal/token"
+)
+
+// Service binds the methods of the null token implementation.
+type Service struct{}
+
+// New returns a new token service
+func New() *Service {
+	return &Service{}
+}
+
+// Generate generates a token with some quirks.  If the claims passed
+// in requests an EntityID of "invalid-token" then an invalid token
+// will be issued.  If the ID is "token-issue-error" then an
+// InternalError will be returned.  For all other values a valid token
+// will be returned.
+func (s *Service) Generate(claims token.Claims, config token.Config) (string, error) {
+	if claims.EntityID == "invalid-token" {
+		return "invalid", nil
+	} else if claims.EntityID == "token-issue-error" {
+		return "", token.ErrInternalError
+	}
+
+	// We do this unchecked as this function will only ever see
+	// prepared values, and we synthetically check for an issuance
+	// error above.
+	st, _ := json.Marshal(claims)
+	return string(st), nil
+}
+
+// Validate checks a token that's been provided.  The token is simply
+// deserialized, there is no validation performed.  Do not use this in
+// production, it is provided as a testing aid only.
+func (s *Service) Validate(t string) (token.Claims, error) {
+	var c token.Claims
+	if err := json.Unmarshal([]byte(t), &c); err != nil {
+		return token.Claims{}, token.ErrTokenInvalid
+	}
+	return c, nil
+}
---
+++ internal/token/null/null_test.go
@@ -0,0 +1,40 @@
+package null
+
+import (
+	"testing"
+
+	"github.com/NetAuth/NetAuth/internal/token"
+)
+
+func TestGenerate(t *testing.T) {
+	tkn := New()
+
+	cases := []struct {
+		claims    token.Claims
+		wantToken string
+		wantErr   error
+	}{
+		{token.Claims{EntityID: "invalid-token"}, "invalid", nil},
+		{token.Claims{EntityID: "token-issue-error"}, "", token.ErrInternalError},
+		{token.Claims{EntityID: "valid"}, "{\"EntityID\":\"valid\",\"Capabilities\":null}", nil},
+	}
+
+	for i, c := range cases {
+		res, err := tkn.Generate(c.claims, token.GetConfig())
+		if err != c.wantErr || res != c.wantToken {
+			t.Errorf("%d: Got %v, %v; Want %v, %v", i, err, res, c.wantErr, c.wantToken)
+		}
+	}
+}
+
+func TestValidate(t *testing.T) {
+	tkn := New()
+
+	if _, err := tkn.Validate("{\"EntityID\":\"valid\",\"Capabilities\":null}"); err != nil {
+		t.Errorf("Couldn't validate real token: %v", err)
+	}
+
+	if _, err := tkn.Validate(""); err != token.ErrTokenInvalid {
+		t.Error("Validated invalid token")
+	}
+}
