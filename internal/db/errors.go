bc00e8123fd91cd51c9edd2b321a2a069640fbdf
Michael Aldridge <aldridge.mac@gmail.com>
2019-11-06 23:24:43 -0800

    internal/db: Fix error string formatting

--- internal/db/errors.go
+++ internal/db/errors.go
@@ -7,21 +7,21 @@ import (
 var (
 	// ErrUnknownEntity is returned for requests to load an entity
 	// that does not exist.
-	ErrUnknownEntity = errors.New("The specified entity does not exist")
+	ErrUnknownEntity = errors.New("the specified entity does not exist")
 
 	// ErrUnknownGroup is returned for requests to load a group
 	// that does not exist.
-	ErrUnknownGroup = errors.New("The specified group does not exist")
+	ErrUnknownGroup = errors.New("the specified group does not exist")
 
 	// ErrUnknownDatabase is returned for an attempt to create a
 	// new database that hasn't been registered.
-	ErrUnknownDatabase = errors.New("The specified database does not exist")
+	ErrUnknownDatabase = errors.New("the specified database does not exist")
 
 	// ErrInternalError is used for all other errors that occur
 	// within a database implementation.
-	ErrInternalError = errors.New("The database has encountered an internal error")
+	ErrInternalError = errors.New("the database has encountered an internal error")
 
 	// ErrBadSearch is returned when a search request cannot be
 	// filled for some reason.
-	ErrBadSearch = errors.New("The provided SearchRequest is invalid")
+	ErrBadSearch = errors.New("the provided SearchRequest is invalid")
 )
