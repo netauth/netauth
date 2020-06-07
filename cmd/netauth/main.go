3ee53f7851023c785f64ac108086a390c82248b5
Michael Aldridge <aldridge.mac@gmail.com>
2020-06-06 22:41:09 -0700

    cmd/netauth: Set default token cache as filesystem

--- cmd/netauth/main.go
+++ cmd/netauth/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/hashicorp/go-hclog"
+	"github.com/spf13/viper"
 
 	"github.com/netauth/netauth/internal/ctl"
 
@@ -17,6 +18,10 @@ var (
 )
 
 func main() {
+	// This runs here so we can reset the defaults that are set
+	// during various init() methods.
+	viper.SetDefault("token.cache", "fs")
+
 	level, set := os.LookupEnv("NETAUTH_VERBOSE")
 	if !set {
 		hclog.SetDefault(hclog.NewNullLogger())
