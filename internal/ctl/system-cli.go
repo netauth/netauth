cb7cfa5bc0530e43fdbc227783187a141d857139
classabbyamp <dev@placeviolette.net>
2022-09-24 17:40:11 -0400

    internal/ctl/system-ctl: don't run initialize(), rename to system-cli
    the default initialize() is not necessary when generating completions
    and docs, so we can override it with an empty function.
    
    Also, the file was renamed to `system-cli` to put it more inline with
    other files' naming conventions.

--- internal/ctl/system-cli.go
+++ internal/ctl/system-cli.go
@@ -8,9 +8,12 @@ var (
 	cliCmd = &cobra.Command{
 		Use:   "cli",
 		Short: "Extra utilities for the CLI",
+		PersistentPreRun: cli_initialize,
 	}
 )
 
 func init() {
 	systemCmd.AddCommand(cliCmd)
 }
+
+func cli_initialize(*cobra.Command, []string) {}
